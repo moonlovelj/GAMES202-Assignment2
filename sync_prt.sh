639e25c4adffacccdb8c94d968a90276b11fda6f
Bin Guo <18500056398@163.com>
2021-04-27 20:43:08 +0800

    Rendering unshadowed images

--- sync_prt.sh
+++ sync_prt.sh
@@ -1,3 +1,3 @@
 export RSYNC_RSH ="ssh -T -c aes128-ctr -o Compression = no -x"
 
-rsync -av --progress ./prt/scenes/cubemap ./homework2/assets
+rsync -av --exclude="*.jpg" --progress ./prt/scenes/cubemap ./homework2/assets
